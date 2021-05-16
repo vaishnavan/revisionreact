fbefba1d392d1f2938fc287fbd41912028d596b2
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-05-16 17:02:14 +0530

    imageGal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
+import React from 'react';
+import { myData } from './gallery'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         appname:'myGallery',
+         filterData:[],
+         show:false,
+         search:'',
+         myType:'',
+      }
+    }
+
+    componentDidMount(){
+      this.setState({
+        myType:'all'
+      })
+    }
+
+    handleChange = (e) =>{
+      this.setState({
+        search:e.target.value,
+        show:false
+      })
+    }
+
+    handleClick = (types) => {
+      switch (types) {
+        case 'all':
+          this.setState({
+            filterData: myData,
+            show:true,
+            myType:types,
+          })
+        break;
+        case 'new':
+          this.setState({
+            filterData: myData.filter((filData) => filData.tag === types),
+            show:true,
+            myType:types,
+          })
+          break;
+        case 'free':
+            this.setState({
+              filterData: myData.filter((filData) => filData.tag === types),
+              show:true,
+              myType:types,
+            })
+            break;
+        case 'pro':
+            this.setState({
+              filterData: myData.filter((filData) => filData.tag === types),
+              show:true,
+              myType:types
+            })
+            break;
+        default:
+          break;
+      }
+    }
+
+    // handleSort = (type) =>{
+    //   this.setState({
+    //     filterData: myData.sort((a,b) => a.price > b.price ? 1:-1).filter((data) => data.tag === type)
+    //   })
+    // }
+    
+
+    render(){
+      const {appname, filterData, show, search, myType} = this.state;
+      
+      const searchData = myData.filter((filData) => {
+        return filData.tag.toLowerCase().includes(search.toLowerCase());
+      })
+      return(
+        <>
+          <h3 style={{textAlign:"center"}}>{appname.toUpperCase()}</h3>
+          <div className="myImage_btn">
+            <button  className={ myType === 'all'? 'active':''} onClick={() => this.handleClick("all")} >All</button>
+            <button className={ myType === 'new'? 'active':''} onClick={() => this.handleClick("new")}>New</button>
+            <button className={ myType === 'free'? 'active':''} onClick={() => this.handleClick("free")}>Free</button>
+            <button className={ myType === 'pro'? 'active':''} onClick={() => this.handleClick("pro")}>Pro</button>
+          </div>
+          <div className="myImage_search">
+            <input type="text" placeholder={`${myData.length} images availablle search here...`} onChange={(e) => this.handleChange(e)} />
+            {/* <button onClick={() => this.handleSort("pro")}>Sort by Low price</button> */}
+          </div>
+
+          <div className="image_error">
+            {searchData.length === 0 ? <h3>You search <span style={{color:"blue"}}>{search}</span> not available</h3>: null}
+          </div>
+          {show ?
+          <div className="img_main">
+            {filterData.map((data) => {
+            return(
+              <div key={data.id} className="image_show">
+                <img src={`images/${data.imgName}`} alt="no" />
+                <div className="image_tag">
+                  <span>{data.tag}</span>
+                  {/* <span>{data.price}</span> */}
+                </div>
+              </div>
+            )
+            })}
+          </div>
+          :
+          <div className="img_main">
+            {searchData.map((data) => {
+            return(
+              <div key={data.id} className="image_show">
+                <img src={`images/${data.imgName}`} alt="no" />
+                <div className="image_tag">
+                  <span>{data.tag}</span>
+                  {/* <span>{data.price}</span> */}
+                </div>
+              </div>
+            )
+            })}
+          </div>
+          }
+        </>
+      )
+    }
 }
 
 export default App;
---
+++ src/gallery.js
@@ -0,0 +1,13 @@
+export const myData = [
+    {id:"1", imgName:"img1.jpeg", tag:"free"},
+    {id:"2", imgName:"img2.jpeg", tag:"pro", price:10},
+    {id:"3", imgName:"img3.jpeg", tag:"free"},
+    {id:"4", imgName:"img4.jpeg", tag:"free"},
+    {id:"5", imgName:"img5.jpeg", tag:"pro", price:50},
+    {id:"6", imgName:"img6.jpeg", tag:"pro", price:20},
+    {id:"7", imgName:"img7.jpeg", tag:"free"},
+    {id:"8", imgName:"img8.jpeg", tag:"new"},
+    {id:"9", imgName:"img9.jpeg", tag:"pro", price:30},
+    {id:"10", imgName:"img10.jpeg", tag:"new"},
+    {id:"11", imgName:"img11.jpeg", tag:"free"},
+]
